82e6e38bed931c4b7476b69f46886f61b1a66cdf
Rasmus Dahlkvist <id12rdt@cs.umu.se>
2018-02-16 16:17:54 +0100

    remove

--- server/src/main/kotlin/org/jetbrains/kotlin/demo/auth0/AuthenticateUser.kt
+++ server/src/main/kotlin/org/jetbrains/kotlin/demo/auth0/AuthenticateUser.kt
@@ -7,9 +7,6 @@ package org.jetbrains.kotlin.demo.auth0
 class AuthenticateUser {
 
     fun signUp(){
-        /*val response = Unirest.post("https://rasmusdahlkvist.eu.auth0.com/dbconnections/signup")
-                .header("content-type", "application/json")
-                .body("{\"client_id\": \"WJzh6vMQR10BlZ978HZ33Peo6AKmerxE\",\"email\": \"$('#signup-email').val()\",\"password\": \"$('#signup-password').val()\",\"user_metadata\": {\"name\": \"john\",\"color\": \"red\"}}")
-                .asString()*/
+
     }
 }
